a2164178f09f90462c3d62f27e8bc3be03006b86
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-07-19 21:20:38 +0530

    Add files via upload

---
+++ Leetcode 2824.py
@@ -0,0 +1,14 @@
+Leetcode- 11
+
+2824 Count Pairs Whose Sum is Less than Target
+
+class Solution(object):
+    def countPairs(self, nums, target):
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]<target:
+                    count+=1
+        return count
+    
+        
---
+++ Leetcode 3467.py
@@ -0,0 +1,12 @@
+Leetcode - 12
+
+3467 Transform Array by Parity
+
+class Solution(object):
+    def transformArray(self, nums):
+        for i in range(len(nums)):
+            if nums[i]%2==0:
+                nums[i]=0
+            else:
+                nums[i]=1
+        return sorted(nums)
