0960d529a97ea1a6358dca04092a19e79072bd2c
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-07-23 22:52:17 +0530

    Add files via upload

---
+++ Leetcode283.py
@@ -0,0 +1,11 @@
+Leetcode - 16
+
+283 Move Zeroes
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        i=0
+        for j in range(len(nums)):
+                if nums[j]!=0:
+                    nums[i],nums[j]=nums[j],nums[i]
+                    i+=1                   
