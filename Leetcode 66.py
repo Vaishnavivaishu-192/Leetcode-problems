a669fa8bd5051d393ea94f75f62c9c471b0fba7d
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-07-30 22:55:41 +0530

    Add files via upload

---
+++ Leetcode 66.py
@@ -0,0 +1,12 @@
+Leetcode-23
+
+66 Plus One
+
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        return [1]+digits
