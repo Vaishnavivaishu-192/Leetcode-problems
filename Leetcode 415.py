ab5784802e0b81f18d976fcb6d8f37ad9a4538e3
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-08-06 21:43:30 +0530

    Add files via upload

---
+++ Leetcode 415.py
@@ -0,0 +1,12 @@
+Leetcode-30
+
+415.Add Strings
+
+class Solution(object):
+    def addStrings(self, num1, num2):
+        m=int(num1)
+        n=int(num2)
+        sum=m+n
+        string=str(sum)
+        return string
+        
