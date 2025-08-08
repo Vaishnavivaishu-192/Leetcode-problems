0dc61c81abcc308c0ae8435d98c8311fe0b326c0
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-08-08 21:32:59 +0530

    Add files via upload

---
+++ Leetcode 520.py
@@ -0,0 +1,12 @@
+Leetcode - 32
+
+520 Detect Capital
+
+class Solution(object):
+    def detectCapitalUse(self, word):
+            if word.isupper() or word.islower():
+                return True
+            elif word[0].isupper() and word[1:].islower():
+                return True
+            else:
+                return False 
