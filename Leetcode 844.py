0614a063220a3f78b635a7c8adf9921ac29ebdfc
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-08-11 22:36:59 +0530

    Add files via upload

---
+++ Leetcode 844.py
@@ -0,0 +1,17 @@
+Leetcode - 35
+
+844 Backspace String Compare
+
+class Solution(object):
+    def backspaceCompare(self, s, t):
+        def build(string):
+            stack = []
+            for char in string:
+                if char != "#":
+                    stack.append(char)
+                elif stack:
+                    stack.pop()
+            return "".join(stack)
+        
+        return build(s) == build(t)
+
