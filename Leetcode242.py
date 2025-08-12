496a9f645c1429eaeb64af0b91bae713f42d163f
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-08-12 22:37:20 +0530

    Add files via upload

---
+++ Leetcode242.py
@@ -0,0 +1,13 @@
+Leetcode 36
+
+242 Valid Anagram
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        if len(s) != len(t):
+            return False
+        for char in set(s):
+            if s.count(char) != t.count(char):
+                return False
+        return True    
+            
