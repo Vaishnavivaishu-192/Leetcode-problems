67c34556a44066740af323a88e4b515b2d6576b9
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-09-15 18:57:08 +0530

    Add files via upload

---
+++ Leetcode 1624.py
@@ -0,0 +1,12 @@
+Leetcode - 67
+
+1624 Largest Substring Between Two Equal Characters
+
+class Solution(object):
+    def maxLengthBetweenEqualCharacters(self, s):
+        current = len(s) - 1
+        for i in range(len(s) - 1, 0, -1):
+            for j in range(len(s) - i):
+                if s[j] == s[j + i]:
+                    return i - 1
+        return -1
