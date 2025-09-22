1923799c395b10005b02f1f44935d16c18d3b90b
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-09-22 21:38:55 +0530

    Add files via upload

---
+++ Leetcode 1436.py
@@ -0,0 +1,14 @@
+Leetcode - 74
+
+1436 Destination City
+
+
+class Solution(object):
+    def destCity(self, paths):
+        cities = set()
+        for path in paths:
+            cities.add(path[0])
+        for path in paths:
+            dest = path[1]
+            if dest not in cities:
+                return dest
