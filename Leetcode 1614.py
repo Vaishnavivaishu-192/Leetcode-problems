fbd0d4e6b73fe5cbba6ce168ac774833e94ff5db
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-09-25 20:31:25 +0530

    Add files via upload

---
+++ Leetcode 1614.py
@@ -0,0 +1,17 @@
+Leetcode - 76
+
+1614 Maximum Nesting Depth of the Parenthesis
+
+class Solution:
+    def maxDepth(self, s):
+        count = 0
+        max_num = 0
+        for i in s:
+            if i == "(":
+                count += 1
+                if max_num < count:
+                    max_num = count
+            if i == ")":
+                count -= 1
+        return(max_num)
+        
