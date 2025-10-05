4b12090117b6d79e6346bb38d34e925cc6d52e22
Vaishnavivaishu-192 <137928912+Vaishnavivaishu-192@users.noreply.github.com>
2025-10-05 20:58:32 +0530

    Add files via upload

---
+++ Leetcode 1351.py
@@ -0,0 +1,13 @@
+Leetcode - 86
+
+1351 Count Negative Numbers in a Sorted Matrix
+
+class Solution(object):
+    def countNegatives(self, grid):
+        count=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]<0:
+                    count+=1
+        return count
+        
